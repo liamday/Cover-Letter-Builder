fc53397547b282aac16547dbe568e7cbe7bbbb00
Lime7029 <Lime7029@gmail.com>
2018-11-20 19:01:15 -0800

    add files

--- spotify_search.js
+++ spotify_search.js
@@ -3,13 +3,15 @@ var contextMenuItem = {
     "title": "Spotify Search",
     "contexts": ["selection"]
 };
-chrome.contextMenus.create(contextMenuItem);
+chrome.runtime.onInstalled.addListener(function(){
+    chrome.contextMenus.create(contextMenuItem);
+});
 
 chrome.contextMenus.onClicked.addListener(function(element){
-    if (element.menuItemId== "spotifySearch" && element.selectionText){
-        //alert("spotify:search:"+element.selectionText);
-        var temporaryWindow =window.open("spotify:search:"+element.selectionText, '', 'width=10,height=10');
-        setTimeout(function(){ temporaryWindow.close(); }, 500);
-        
-    }
+        if (element.menuItemId== "spotifySearch" && element.selectionText){
+            //alert("spotify:search:"+element.selectionText);
+            var temporaryWindow =window.open("spotify:search:"+element.selectionText, '', 'width=10,height=10');
+            setTimeout(function(){ temporaryWindow.close(); }, 500);
+
+        }
 });
